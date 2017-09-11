dc75d9fae25ad15d90f23d7ef576e3814118ebb1
Joel Bell <joel@geekbell.com>
2017-09-11 18:53:20 +0000

    Resolved a few bugs in dayOfWeek placement

--- calendar.php
+++ calendar.php
@@ -16,34 +16,34 @@ class calendar {
 
 	function showCalendar($date) {
 
-		echo $date;
+		//echo $date;
 		if (!isset($date)) {
 			$date = mktime();
 		}
-
+		$output = '';
 		//Create Month and Date Header
-		$output .= "<div class='CalendarLabel'>";
+		$output .= "<div class='calendar-Label'>";
 		$output .= date("F", $date);
 		$output .= date("Y", $date);
-		$output .= "</div>";
-		$output .= "<table class='CalendarTable'><tr>";
+		$output .= "</div>\n";
+		$output .= "<table class='calendar-Table'><tr>\n";
 
 		//Display Letter for Days of Week
 		foreach ($this->daysOfWeek as $day) {
-			$output .= "<th class='TableHeader'>$day</th>";
+			$output .= "<th class='calendar-Header'>$day</th>\n";
 		}
 
-		$output .= "</tr><tr>";
+		$output .= "</tr><tr>\n";
 
 		$currentDay = 1;
-		//Get the day of week for first day of month.
-		//$today = mktime(0, 0, 0, date("m")  , 1, date("Y"));
-		$dayOfWeek = date("w", $date);
-		
+		//Get the day of week for the first day of month.
+		$firstOfTheMonth = mktime(0, 0, 0, date("m", $date), 1, date("Y", $date));
+		$dayOfWeek = date("w", $firstOfTheMonth);
+		echo $dayOfWeek;
 		//Create blank days until the first day of the month.
 		if ($dayOfWeek > 0) {
 			for ($i = 1; $i <= $dayOfWeek; $i++) {
-				$output .="<td class='CalendarDate'>&nbsp;</td>";
+				$output .="<td class='calendar-Date'>&nbsp;</td>\n";
 			}
 		}
 
@@ -51,16 +51,16 @@ class calendar {
 		while ($currentDay <= $this->numberOfDays) {
 
 			if ($dayOfWeek == 7) {
-				$output .= "</tr><tr>";
+				$output .= "</tr><tr>\n";
 				$dayOfWeek = 0;
 			}
 
-			$output .= "<td>$currentDay</td>";
+			$output .= "<td>$currentDay</td>\n";
 			$currentDay++;
 			$dayOfWeek++;
 
 		}
-		$output .= "</tr></table>";
+		$output .= "</tr></table>\n";
 
 		return $output;
 
@@ -68,8 +68,8 @@ class calendar {
 
 }
 
-$cal = new calendar();
-//$date = mktime(0,0,0,12,1,2017);
-echo $cal->showCalendar($date);
+//$cal = new calendar();
+//$date = mktime(0,0,0,6,1,2017);
+//echo $cal->showCalendar($date);
 
 ?>
